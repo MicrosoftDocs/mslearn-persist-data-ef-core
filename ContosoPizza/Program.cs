1375433f6473bc34b606348907f520ef7b72d7a3
Cam Soper <cam.soper@microsoft.com>
2021-12-07 18:13:16 -0600

    Update Program.cs

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -9,6 +9,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+// Add the AddDbContextCode
+
 builder.Services.AddScoped<PizzaService>();
 
 var app = builder.Build();
