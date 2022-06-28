bad11c3feae8cfe5c520e1da152475ea0c8efcfa
Nick M <nmoeller96@hotmail.com>
2022-06-27 21:48:42 -0500

    save 3

--- ContosoPizza/Migrations/20220628024153_ModelRevisions.Designer.cs
+++ ContosoPizza/Migrations/20220628024153_ModelRevisions.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ContosoPizza.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContosoPizza.Migrations
 {
     [DbContext(typeof(PizzaContext))]
-    partial class PizzaContextModelSnapshot : ModelSnapshot
+    [Migration("20220628024153_ModelRevisions")]
+    partial class ModelRevisions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.6");
--- ContosoPizza/Models/Pizza.cs
+++ ContosoPizza/Models/Pizza.cs
@@ -1,12 +1,15 @@
 namespace ContosoPizza.Models;
+using System.ComponentModel.DataAnnotations;
 
 public class Pizza
 {
     public int Id { get; set; }
 
+    [Required]
+    [MaxLength(100)]
     public string? Name { get; set; }
 
     public Sauce? Sauce { get; set; }
-    
+
     public ICollection<Topping>? Toppings { get; set; }
 }
